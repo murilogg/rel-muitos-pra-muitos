ae75c5f3e61c25f6e04f664f9d3cbb87653d3cba
murilo gomes dos santos <murilo29@hotmail.com.br>
2019-02-22 18:17:51 -0300

    initial commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,26 @@
 	<dd>
 		<a href="/projetos_desenvolvedores">Saiba mais</a>
 	</dd>
+	<hr>
+	<dt><b>ALOCAR DESENVOLVEDOR A UM PROJETO -> attach(3, [ 'horas_semanais' => 40 ])</b></dt>
+	<dd>
+		<a href="/alocardesenvolvedor">Saiba mais</a>
+	</dd>
+	<hr>
+	<dt><b>ALOCAR PROJETO A UM DESENVOLVEDOR -> attach(2, [ 'horas_semanais' => 40 ])</b></dt>
+	<dd>
+		<a href="/alocarprojeto">Saiba mais</a>
+	</dd>
+	<hr>
+	<dt><b>DESALOCAR DESENVOLVEDOR DE UM PROJETO -> detach([1,2,3])</b></dt>
+	<dd>
+		<a href="/desalocardesenvolvedor">Saiba mais</a>
+	</dd>
+	<hr>
+	<dt><b>DESALOCAR PROJETO DE UM DESENVOLVEDOR -> detach([1,2,3])</b></dt>
+	<dd>
+		<a href="/desalocarprojeto">Saiba mais</a>
+	</dd>
 </dl>
 </h3>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/desenvolvedores_projetos', function () {
 	$des = Desenvolvedor::with('projetos')->get();
 
     foreach ($des as $p) {
+    	echo "<p>ID: <b>" . $p->id . "</b></p>";
     	echo "<p>Nome do Desenvolvedor: <b>" . $p->nome . "</b></p>";
     	echo "<p>Cargo: <b>". $p->cargo ."</b></p>";
     	if(count($p->projetos) > 0){
@@ -23,6 +24,8 @@ Route::get('/desenvolvedores_projetos', function () {
     	    	echo "Horas Trabalhadas: " . $d->pivot->horas_semanais . " hr</li>";
     		}
     		echo "</ul>";
+    	}else{
+    		echo "Nenhum Projeto com este Desenvolvedor";
     	}
     	echo "<hr>";
     }
@@ -33,6 +36,7 @@ Route::get('/projetos_desenvolvedores', function () {
 	$prod = Projeto::with('desenvolvedores')->get();
 
 	foreach ($prod as $p) {
+		echo "<p>ID: <b>" . $p->id . "</b></p>";
 		echo "<p>Nome do Projeto: <b>" . $p->nome . "</b></p>";
 		echo "<p>Horas no Projeto: <b>" . $p->estimativa_horas . " hr</b></p>";
 		if(count($p->desenvolvedores) > 0){
@@ -51,3 +55,58 @@ Route::get('/projetos_desenvolvedores', function () {
 	}
     //return $prod->toJson();
 });
+
+Route::get('/alocardesenvolvedor', function(){
+	$proj = Projeto::find(3);
+	if(isset($proj)){
+		$proj->desenvolvedores()->attach(3, [ 'horas_semanais' => 40 ]);
+	}
+	echo "Desenvolvedor alocado com sucesso";
+});
+
+Route::get('/alocarprojeto', function(){
+	$proj = Desenvolvedor::find(1);
+	if(isset($proj)){
+		$proj->projetos()->attach(2, [ 'horas_semanais' => 40 ]);
+	}
+	echo "Projeto alocado com sucesso";
+});
+
+Route::get('/desalocardesenvolvedor', function(){
+	$proj = Projeto::find(3);//PROJETO_ID
+	if(isset($proj)){
+		$proj->desenvolvedores()->detach(1);//DESENVOLVEDOR_ID
+		echo "Desenvolvedor desalocado com sucesso";
+	}else{
+		echo "ERRO DESENVOLVEDOR NÃO ENCONTRADO";
+	}
+	
+});
+
+Route::get('/desalocarprojeto', function(){
+	$proj = Desenvolvedor::find(3); // DESENVOLVEDOR_ID
+	if(isset($proj)){
+		$proj->projetos()->detach(4);// PROJETO_ID
+		echo "Projeto desalocado com sucesso";
+	}else{
+		echo "ERRO PROJETO NÃO ENCONTRADO";
+	}
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
